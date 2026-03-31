1f00cc0ef27720682f5266591a15bf4116c26d7a
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-03-31 19:14:45 +0300

    Go to Alıştırma

---
+++ 03 Mart 05/03 Mart 05/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mart032
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("A,B,C,D Hangisi:");
+            string harf = Console.ReadLine();
+            switch (harf)
+            {
+                case "A":
+                case "a":
+                    Console.WriteLine("A seçtiniz");
+                    break;
+                case "B":
+                case "b":
+                    Console.WriteLine("B seçtiniz");
+                    break;
+                case "C":
+                case "c":
+                    Console.WriteLine("C seçtiniz");
+                    goto case "A";
+                case "D":
+                case "d":
+                    Console.WriteLine("D seçtiniz");
+                    goto case "B";
+                default:
+                    Console.WriteLine("Geçersiz harf girdiniz.");
+                    break;
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
