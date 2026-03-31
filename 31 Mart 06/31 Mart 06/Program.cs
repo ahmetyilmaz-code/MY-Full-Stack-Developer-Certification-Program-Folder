20bef929a145ff539af48968ff286d5438bc9f8e
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-03-31 23:44:45 +0300

    Rastgele sayı  ondalıklı Min=0.0 Max=0.99 1  değil
    // Rastgele sayı üretme, ondalıklı Min=0.0 Max=0.9999 1 dahil değil

---
+++ 31 Mart 06/31 Mart 06/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31_Mart_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Rastgele sayı üretme, ondalıklı Min=0.0 Max=0.9999 1 dahil değil
+
+
+            Random RastgeleSayi = new Random();
+            // random sınıf int sayılar üretir genel olarak.
+
+            double rastgele = RastgeleSayi.NextDouble();
+            // Burada 0 ile 1 arasında ondalıklı sayı üretir 0 dahil ve 1 dahil değil
+            Console.WriteLine(rastgele);
+
+
+
+        }
+    }
+}
