a22d3c5373f20738a92434415a004189f5cfc51c
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-03-31 19:05:47 +0300

    // For Dögüsü
    // For Dögüsü
                // Döngüler tekrar eden işlemleri yapmamızı sağlar.
                // Döngüler sayesinde kodlarımız daha az ve daha okunabilir olur.
                // Döngüler 3 çeşittir. For, While, Do-While
                // For en temel döngüdür. Genellikle sayısal işlemlerde kullanılır.

---
+++ 03 Mart 01/03 Mart 01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mart032
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // For Dögüsü
+            // Döngüler tekrar eden işlemleri yapmamızı sağlar.
+            // Döngüler sayesinde kodlarımız daha az ve daha okunabilir olur.
+            // Döngüler 3 çeşittir. For, While, Do-While
+            // For en temel döngüdür. Genellikle sayısal işlemlerde kullanılır.
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Merhaba");
+            }
+
+
+
+
+        }
+    }
+}
