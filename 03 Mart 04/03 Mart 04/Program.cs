1fc5c1ea61149b0d1b9af05f6b91aada2943bf8c
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-03-31 19:13:08 +0300

    Girilen ifadeyi ekrana 10 defa yazan program

---
+++ 03 Mart 04/03 Mart 04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mart032
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //kullanının girdiği ifadeyi ekrana 10 defa yazdıran program
+
+            Console.Write("Lütfen bir ifade giriniz: ");
+            string ifade = Console.ReadLine();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(ifade);
+            }
+
+        }
+    }
+}
