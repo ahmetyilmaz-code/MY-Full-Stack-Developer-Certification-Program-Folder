3e2d8b8c0f2e8369277c8d2907841e3cdceaedc3
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-03-31 23:29:15 +0300

    goto - label alıştırması
     kullanıcı negatif değer girene kadar sayıları toplayan program. goto - label ile

---
+++ 31 Mart 01/31 Mart 01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31_Mart_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // kullanıcı negatif değer girene kadar sayıları toplayan program. goto - label ile
+            int toplam = 0;
+            int i = 1;
+        index:
+            Console.Write($"{i}.sayıyı giriniz:");
+            i++;
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            if (sayi > 0)
+            {
+                toplam += sayi;
+                goto index;
+            }
+            Console.WriteLine($"Toplam ={toplam}");
+
+        }
+    }
+}
