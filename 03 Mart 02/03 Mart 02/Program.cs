0d0fd3dc4d7b19ca32cc6eb404197f484b580f65
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-03-31 19:09:00 +0300

    Girilen ifadeyi  girilen sayı kadar yazma

---
+++ 03 Mart 02/03 Mart 02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mart032
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //kullanının girdiği ifadeyi kullanıcının girdiği sayı kadar yazdıran program
+
+            Console.Write("Lütfen bir ifade giriniz: ");
+            string ifade = Console.ReadLine();
+            Console.Write("Bu ifadeyi kaç kere yazdırmak istiyorsunuz? :");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < sayi; i++)
+            {
+                Console.WriteLine(ifade);
+            }
+
+        }
+    }
+}
