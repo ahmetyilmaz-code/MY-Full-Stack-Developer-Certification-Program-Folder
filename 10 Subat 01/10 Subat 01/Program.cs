bae7f82dc2cad607ff61d4e57e5e93407c758dcb
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-03-31 21:30:04 +0300

    Sayı iteme ve kare alan program
    // kullanıcıdan sayı istenilerek istenilen sayının karesini hesaplayan program

---
+++ 10 Subat 01/10 Subat 01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10subat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // kullanıcıdan sayı istenilerek istenilen sayının karesini hesaplayan program
+
+            string sayiTxt;
+            Console.Write("Bir Sayi Giriniz :");
+            sayiTxt = Console.ReadLine();
+            int sayi = Convert.ToInt32(sayiTxt);
+
+            int karesi = sayi * sayi;
+            Console.WriteLine($"Girdiniz sayı  :{sayi} \nSayının Karesi :{karesi}");
+
+        }
+    }
+}
---
+++ 10 Subat 01/10 Subat 01/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("10 Subat 01")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("10 Subat 01")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("f56b79b0-dead-48c1-9e94-39c07bc1febe")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
