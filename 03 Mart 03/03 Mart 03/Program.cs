b26183339232e879f629acd6797430a22f9027f7
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-03-31 19:11:22 +0300

    Girilen sayı kadar virgüllü sayı yazan program
    //kullanıcının girdiği sayı kadar ekrana virgül ise sayıları yazan program
    // 5
    // 1, 2, 3, 4, 5

---
+++ 03 Mart 03/03 Mart 03/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mart032
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //kullanının girdiği sayı kadar ekrana virül ise sayıları yazan program
+            // 5
+            // 1, 2, 3, 4, 5
+
+            Console.WriteLine("Girilen sayıya kadar sayıları yazan program");
+            Console.Write("Kaça kadar sayıları yazayım :");
+            int adet = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= adet; i++)
+            {
+                if (i < adet)
+                {
+                    Console.Write($"{i},");
+                }
+                else
+                {
+                    Console.Write(i);
+                }
+
+            }
+
+        }
+    }
+}
