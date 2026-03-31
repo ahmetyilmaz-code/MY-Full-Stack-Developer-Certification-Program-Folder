8d07714d039598f8a8bbdb563f4809aa944e8632
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-03-31 19:18:45 +0300

    Girilen sayıya göre günü hesaplayan program
    // kullanıcıdan gün sayısı alarak haftanın hangi günü olduğunu bulan program.
    // bugün günlerden pazartesi. Kaç gün sonrasını merak ediyorsun?

---
+++ 03 Mart 06/03 Mart 06/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mart31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // kullanıcıdan gün sayıyısı alarak haftanın hang günü olduğunu bulan program.
+            // bugün günlerden pazartesi. Kaç gün sonrasını merak ediyorsun?
+
+            Console.Write("Bugün günlerden pazartesi. Kaç gün sonrasını merak ediyorsunuz? :");
+            int gun = Convert.ToInt32(Console.ReadLine());
+            string sonuc = $"{gun} sonra günlerden ";
+
+            switch (gun % 7)
+            {
+                case 0:
+                    sonuc += "Pazartesi";
+                    break;
+                case 1:
+                    sonuc += "Salı";
+                    break;
+                case 2:
+                    sonuc += "Çarşamba";
+                    break;
+                case 3:
+                    sonuc += "Perşembe";
+                    break;
+                case 4:
+                    sonuc += "Cuma";
+                    break;
+                case 5:
+                    sonuc += "Cumartesi";
+                    break;
+                case 6:
+                    sonuc += "Pazar";
+                    break;
+            }
+            Console.WriteLine(sonuc);
+
+        }
+    }
+}
