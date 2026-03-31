c58b1b2657a49606a4f9bc5f8287d35e23806cdc
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-03-31 23:57:51 +0300

    Yazı Tura Oyunu cansız
    // Yazı Tura Oyunu
                // Nasıl Oynanır ?
                // önce kullanıcıya Yazı(y)mı Tura(T)mı diye sorulur.
                // sonra rastgele bir şekilde yazı veya tura seçilir.
                // gelen sonuç ile kullanıcının girdiği sonucu kıyaslayarak
                // kazandın veya kaybettin yazar.

---
+++ 31 Mart 09/31 Mart 09/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31_Mart_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Yazı Tura Oyunu
+            // Nasıl Oynanır ?
+            // önce kullanıcıya Yazı(y)mı Tura(T)mı diye sorulur.
+            // sonra rastgele bir şekilde yazı veya tura seçilir.
+            // gelen sonuç ile kullanıcının girdiği sonucu kıyaslayarak
+            // kazandın veya kaybettin yazar.
+
+            Console.WriteLine("Yazı Tura Oyununa Hoşgeldiniz...");
+            Console.WriteLine("Yazı için (Y), Tura için (T) seçiniz ?");
+            char seçim = Console.ReadKey().KeyChar;
+
+            Random rnd = new Random();
+            int yazıtura = rnd.Next(1, 3); // 1 yazı demek, 2 tura demek
+            Console.WriteLine(yazıtura);
+            if (seçim == 'y' || seçim == 'Y')
+            {
+                if (yazıtura == 1)
+                {
+                    Console.WriteLine("Sen Yazı Ben Yazı Kazandınız...");
+                }
+                else
+                {
+                    Console.WriteLine("Sen Yazı Ben Tura Kaybettiniz...");
+                }
+            }
+            else if (seçim == 'T' || seçim == 't')
+            {
+                if (yazıtura == 2)
+                {
+                    Console.WriteLine("Sen Tura Ben Tura Kazandınız...");
+                }
+                else
+                {
+                    Console.WriteLine("Sen Tura Ben Yazı Kaybettiniz...");
+                }
+            }
+            else
+            {
+                Console.WriteLine("ya yazı seçiniz yada tura başka bişi değil.");
+            }
+
+        }
+    }
+}
---
+++ 31 Mart 09/31 Mart 09/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("31 Mart 09")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("31 Mart 09")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("2955bdb6-78bb-4e4b-aab4-bad11d52ae1c")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
