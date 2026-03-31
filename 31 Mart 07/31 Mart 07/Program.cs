91b4047003ddbc6b359da6644198cf52c1c45d59
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-03-31 23:46:57 +0300

    rastgele sayılardan 7ye tam bölünen 50 sayıkontrol
    // En fazla 50 tane olacak şekilde rastgele sayıda rastgele sayı üreten program.
    // 7ye tam bölünen sayıyı bulanakadar rasgele sayı üreten program

---
+++ 31 Mart 07/31 Mart 07/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31_Mart_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // En fazla 50 tane olacak şekilde rastgele sayıda rastgele sayı üreten program.
+            // 7ye tam bölünen sayıyı bulanakadar rasgele sayı üreten program
+            int i = 1;
+            Random RastgeleSayi = new Random();
+            while (true)
+            {
+                int sayi = RastgeleSayi.Next();
+                Console.WriteLine($"{i}. sayi :{sayi}");
+                i++;
+                if (sayi % 7 == 0)
+                {
+                    Console.WriteLine($"{i}.Sayı olan {sayi} sayı 7ye tam bölünür. ");
+                    break;
+                }
+                else if (i == 50)
+                {
+                    Console.WriteLine("Max 50 denemeye ulaşıldı.");
+                    break;
+                }
+
+            }
+
+
+        }
+    }
+}
---
+++ 31 Mart 07/31 Mart 07/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("31 Mart 07")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("31 Mart 07")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("a82a5ed1-a5dc-49c1-9375-dda7c21caabf")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
