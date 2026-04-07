490bbda796c4ac0fc3090b3ebb2e02d68829cfb0
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-07 19:34:38 +0300

    rastgele 10 haneli şifre 4Bhrf 3khrf 2rkm 1noktlma
    // kullanıcıya Tekrar (T) tuşuna bastığı sürece rastgele karakterleden oluşan 10 basamaklı şifre öneren program.
                // Şifre: asdRfv!'15
                // Yeni şifre için T tuşuna basın...
                // başka tuşta şifre kabul edildi program kapatılıyor...
    
                // 4 tanesi büyük harf --> 65-90 (1)
                // 3 tanesi küçük harf --> 97-122 (2)
                // 2 tanesi rakam --> 48-57 (2)
                // 1 tanesi noktalama işareti olsun karakterler --> !  #  $  %  &  +  -  *  /  .  ;  :  ? (4)
                //                                              --> 33 35 36 37 38 43 45 42 47 46 58 59 63
                //                                              --> 33 35-38 42,43 45-47 58,59 63

---
+++ 02 Nisan 06/02 Nisan 06/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Nisan_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // kullanıcıya Tekrar (T) tuşuna bastığı sürece rastgele karakterleden oluşan 10 basamaklı şifre öneren program.
+            // Şifre: asdRfv!'15
+            // Yeni şifre için T tuşuna basın...
+            // başka tuşta şifre kabul edildi program kapatılıyor...
+
+            // 4 tanesi büyük harf --> 65-90 (1)
+            // 3 tanesi küçük harf --> 97-122 (2)
+            // 2 tanesi rakam --> 48-57 (2)
+            // 1 tanesi noktalama işareti olsun karakterler --> !  #  $  %  &  +  -  *  /  .  ;  :  ? (4)
+            //                                              --> 33 35 36 37 38 43 45 42 47 46 58 59 63
+            //                                              --> 33 35-38 42,43 45-47 58,59 63
+
+            Random rnd = new Random();
+            char cevap = 'T';
+            string sifre = "";
+            int buyukharf = 0, kucukharf = 0, rakam = 0, noktalama = 0;
+            while (cevap == 'T' || cevap == 't')
+            {
+                sifre = "";
+                int sayi = 0;
+                while ((buyukharf + kucukharf + rakam + noktalama) < 10)
+                {
+                    int karakterturu = rnd.Next(1, 5);
+                    switch (karakterturu)
+                    {
+                        case 1:
+                            if (buyukharf == 4)
+                            {
+                                continue;
+                            }
+                            sayi = rnd.Next(65, 91);
+                            sifre += (char)sayi;
+                            buyukharf++;
+                            break;
+
+                        case 2:
+                            if (kucukharf == 3)
+                            {
+                                continue;
+                            }
+                            sayi = rnd.Next(97, 123);
+                            sifre += (char)sayi;
+                            kucukharf++;
+                            break;
+
+                        case 3:
+                            if (rakam == 2)
+                            {
+                                continue;
+                            }
+                            sayi = rnd.Next(48, 58);
+                            sifre += (char)sayi;
+                            rakam++;
+                            break;
+
+                        default:
+                            if (noktalama == 1)
+                            {
+                                continue;
+                            }
+
+                            while (noktalama != 1)
+                            {
+                                //!  #  $  %  &  +  -  *  /  .  ;  :  ?
+                                sayi = rnd.Next(33, 64);
+                                switch (sayi)
+                                {
+                                    case '!':
+                                    case '#':
+                                    case '$':
+                                    case '%':
+                                    case '&':
+                                    case '+':
+                                    case '-':
+                                    case '*':
+                                    case '/':
+                                    case '.':
+                                    case ':':
+                                    case ';':
+                                    case '?':
+                                        sifre += (char)sayi;
+                                        noktalama++;
+                                        break;
+                                }
+
+                            }
+                            break;
+                    }
+                }
+                buyukharf = 0;
+                kucukharf = 0;
+                rakam = 0;
+                noktalama = 0;
+                Console.WriteLine();
+                Console.WriteLine($"Şifre : {sifre}");
+                Console.Write("Tekrar şifre için (T):");
+                cevap = Console.ReadKey().KeyChar;
+            }
+            Console.WriteLine("\nEn son yazılan şifre onaylanmıştır program kapatılıyor...");
+            Console.WriteLine($"Seçilen Şifre : {sifre}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02 Nisan 06/02 Nisan 06/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("02 Nisan 06")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("02 Nisan 06")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("ce883926-187b-4bca-99e2-456e01b27f80")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
