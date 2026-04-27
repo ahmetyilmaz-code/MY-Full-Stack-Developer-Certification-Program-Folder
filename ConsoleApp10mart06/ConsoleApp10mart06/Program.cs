42041ad9b54c7a11f8a2c9944cc77c121ed98354
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-27 18:46:09 +0300

    while ile 1-5 sayıları yazma
    // While ile 1 ile 5 arasındaki sayıları ekrana yazdıran bir program yazınız.

---
+++ ConsoleApp10mart06/ConsoleApp10mart06/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleAppDers10mart06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // While ile 1 ile 5 arasındaki sayıları ekrana yazdıran bir program yazınız.
+            int sayi = 1;
+            while (sayi < 6)
+            {
+                Console.WriteLine(sayi);
+                sayi++;
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
