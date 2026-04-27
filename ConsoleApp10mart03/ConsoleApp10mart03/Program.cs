0494e7c18d2bf6fcc36605dcd2bcf76c5da7749e
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-27 18:44:54 +0300

    girilen tek ve çift sayıalrı toplama
    // kullanıcıdan alınan sayı aralığındaki tek ve çift sayıların toplamanı bulan program
                // tek sayıların toplamı :
                // çift sayıların toplamı :

---
+++ ConsoleApp10mart03/ConsoleApp10mart03/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mart1003
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // kullanıcıdan alınan sayı aralığındaki tek ve çift sayıların toplamanı bulan program
+            // tek sayıların toplamı :
+            // çift sayıların toplamı :
+
+            Console.Write("Başlangıç: ");
+            int bas = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Bitiş: ");
+            int bit = Convert.ToInt32(Console.ReadLine());
+
+            if (bas > bit)
+            {
+                int baslangic = bas;
+                bas = bit;
+                bit = baslangic;
+            }
+            int tekToplam = 0;
+            int ciftToplam = 0;
+
+            for (int i = bas; i <= bit; i++)
+            {
+                Console.WriteLine($"İncelenen sayı: {i}");
+                if (i % 2 == 1)
+                {
+                    tekToplam += i;
+                    Console.WriteLine($"Tek sayıların ara toplamı: {tekToplam}");
+                }
+                else
+                {
+                    ciftToplam += i;
+                    Console.WriteLine($"Çift sayıların ara toplamı: {ciftToplam}");
+                }
+                Console.WriteLine("-----------------------------");
+
+            }
+            Console.WriteLine($"Tek sayıların toplamı: {tekToplam}");
+            Console.WriteLine($"Çift sayıların toplamı: {ciftToplam}");
+        }
+    }
+}
