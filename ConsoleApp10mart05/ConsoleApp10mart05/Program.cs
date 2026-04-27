f70ff4a1b215a1296bff6ba1a43dde7ccdc6bb26
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-27 18:45:38 +0300

    while döngüsü deneme

---
+++ ConsoleApp10mart05/ConsoleApp10mart05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleAppDers10mart05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            while (a < b) // a b den küçük olduğu sürece döngü devam eder.
+            {
+                Console.WriteLine("b a den büyüktür");
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
