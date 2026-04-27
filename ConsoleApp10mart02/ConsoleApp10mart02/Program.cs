833c855bdb94c0f744909ba57e39a6c61b796294
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-27 18:44:30 +0300

    for döngüsü ile örüntü
    /*
                For döngüsü ile aşağadaki gibi üçgen çizen program
    
                 *
                ***
               *****
              *******
             *********
              *******
               *****
                ***
                 * 
                 
                 */

---
+++ ConsoleApp10mart02/ConsoleApp10mart02/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ConsoleAppDers10mart02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            For döngüsü ile aşağadaki gibi üçgen çizen program
+
+             *
+            ***
+           *****
+          *******
+         *********
+          *******
+           *****
+            ***
+             * 
+             
+             */
+
+            Console.Write("Kaç basamak: ");
+            int basamak = Convert.ToInt32(Console.ReadLine());
+
+
+
+            for (int i = 1; i <= basamak; i++)
+            {
+                for (int j = 1; j <= basamak - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 1; i <= basamak; i++)
+            {
+                for (int j = 1; j <= i - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= 2 * (basamak - i) + 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
