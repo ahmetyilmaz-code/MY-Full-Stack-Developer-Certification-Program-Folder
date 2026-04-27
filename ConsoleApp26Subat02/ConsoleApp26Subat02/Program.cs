3ceb6ab719ebb2a07d5a0c7a9248cf5203dbee4c
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-27 18:28:46 +0300

    onay için O veya o basma

---
+++ ConsoleApp26Subat02/ConsoleApp26Subat02/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace subat2602
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Onaylamak için O tuşuna basınız. Red için R tuşuna basınız.");
+            Console.WriteLine("=>");
+            char cevap = Console.ReadKey().KeyChar;
+
+            if (cevap == 'O' || cevap == 'o')
+            {
+                Console.WriteLine("\nOnaylandı.");
+            }
+            else if (cevap == 'R' || cevap == 'r')
+            {
+                Console.WriteLine("\nRed edildi.");
+            }
+            else
+            {
+                Console.WriteLine("\nGeçersiz giriş.");
+            }
+
+            switch (cevap)
+            {
+                case 'O':
+                case 'o':
+                    Console.WriteLine("\nOnaylandı.");
+                    break;
+                case 'R':
+                case 'r':
+                    Console.WriteLine("\nRed edildi.");
+                    break;
+                default:
+                    Console.WriteLine("\nGeçersiz giriş.");
+                    break;
+            }
+
+
+
+
+        }
+    }
+}
