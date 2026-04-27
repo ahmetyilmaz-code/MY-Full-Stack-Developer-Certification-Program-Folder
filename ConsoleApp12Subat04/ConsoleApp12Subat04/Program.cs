4030220b1d8b3727b52162a8ae9e68ce700f38ac
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-27 18:16:13 +0300

    100den büyükküçük

---
+++ ConsoleApp12Subat04/ConsoleApp12Subat04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12Subat04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //kullanıcıdan aldığı sayının 100 den büyük olup olmadığını kontrol eden program
+            Console.WriteLine("Lütfen bir sayı giriniz:");
+            Console.Write("Sayı: ");
+            string inputTxt = Console.ReadLine();
+            int input = Convert.ToInt32(inputTxt);
+
+            string sonuc = input > 100 ? "sayı 100den büyüktür" : "sayı 100den büyük değildir";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
