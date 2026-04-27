cb56af1660ca4eeb9c90a90bf2a6dbc46dbc9038
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-27 18:22:07 +0300

    2 sayın alarak k-b yapan prgram
    // kullanıcıdan 2 sayı alarak büyük sayıdan kücük sayıyı
     // çıkartarak sonucu ekrana yazdıran program

---
+++ ConsoleApp19Subat01/ConsoleApp19Subat01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace subat1901
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // kullanıcıdan 2 sayı alarak büyük sayıdan kücük sayıyı
+            // çıkartarak sonucu ekrana yazdıran program
+
+            Console.WriteLine("Lütfen 2 sayı giriniz:");
+            Console.Write("Birinci sayıyı giriniz:");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("İkinci sayıyı giriniz:");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi1 > sayi2)
+            {
+                Console.WriteLine("Sonuç: " + (sayi1 - sayi2));
+            }
+            else if (sayi2 > sayi1)
+            {
+                Console.WriteLine("Sonuç: " + (sayi2 - sayi1));
+            }
+            else
+            {
+                Console.WriteLine("Girdiğiniz sayılar eşit.");
+
+            }
+        }
+    }
+}
