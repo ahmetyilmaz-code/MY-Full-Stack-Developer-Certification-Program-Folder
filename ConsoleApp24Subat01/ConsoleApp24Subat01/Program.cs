e089874b5d72a980914c236f124a9cd7acfb7125
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-27 18:24:50 +0300

    // Basit hesap makinesi
    // Basit hesap makinesi
    // Kullanıcıdan iki sayı ve bir işlem türü alarak sonucu hesaplayan bir program yazınız.
    // Sayı 1 : 20
    // Sayı 2 : 40
    // İşlem Türü : (+,-,*,/) : +
    // 20 + 40 = 60

---
+++ ConsoleApp24Subat01/ConsoleApp24Subat01/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace subat2401
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Basit hesap makinesi
+            // Kullanıcıdan iki sayı ve bir işlem türü alarak sonucu hesaplayan bir program yazınız.
+            // Sayı 1 : 20
+            // Sayı 2 : 40
+            // İşlem Türü : (+,-,*,/) : +
+            // 20 + 40 = 60
+            Console.WriteLine("Basit Hesap Makinesi");
+            Console.WriteLine("Lütfen iki sayı ve bir işlem türü giriniz.");
+            Console.Write("Sayı 1 : ");
+            double sayi1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Sayı 2 : ");
+            double sayi2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("İşlem Türü : (+,-,*,/) : ");
+            string islemTuru = Console.ReadLine();
+            Console.Write($"{sayi1} {islemTuru} {sayi2} = ");
+
+
+            if (islemTuru != "+" || islemTuru != "-" || islemTuru != "*" || islemTuru != "/")
+            {
+                Console.WriteLine("Geçersiz işlem türü");
+            }
+            else
+            {
+
+                if (islemTuru == "+")
+                {
+                    Console.Write(sayi1 + sayi2);
+                }
+                else if (islemTuru == "-")
+                {
+                    Console.Write(sayi1 - sayi2);
+                }
+                else if (islemTuru == "*")
+                {
+                    Console.Write(sayi1 * sayi2);
+                }
+                else if (islemTuru == "%")
+                {
+                    Console.Write(sayi1 % sayi2);
+                }
+                else if (islemTuru == "/")
+                {
+                    if (sayi2 != 0)
+                    {
+                        Console.Write(sayi1 / sayi2);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sıfıra bölme hatası!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Geçersiz işlem türü");
+                }
+
+            }
+        }
+    }
+
+}
