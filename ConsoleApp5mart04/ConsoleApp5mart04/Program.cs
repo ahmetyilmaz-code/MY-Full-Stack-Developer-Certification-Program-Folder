9cf46bd18081223b61ad2e6bd3416641839a884a
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-27 18:37:26 +0300

    girilen aralıktaki çift sayı yazma
    // kullanıcının girdiği aralıktaki sayıların içinden çift olanlarını ekrana yazan program
                // başlangıç : 1
                // bitiş : 6
                // çıktı : 2, 4, 6

---
+++ ConsoleApp5mart04/ConsoleApp5mart04/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mart504
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // kullanıcının girdiği aralıktaki sayıların içinden çift olanlarını ekrana yazan program
+            // başlangıç : 1
+            // bitiş : 6
+            // çıktı : 2, 4, 6
+
+            Console.WriteLine("Lütfen bir sayı aralığının başlangıç ve bitiş aralığını giriniz");
+            Console.Write("Başlangıç : ");
+            int baslangic = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Bitiş : ");
+            int bitis = Convert.ToInt32(Console.ReadLine());
+
+            if (baslangic > bitis)
+            {
+                int _baslangıc = baslangic;
+                baslangic = bitis;
+                bitis = _baslangıc;
+            }
+
+            for (int i = baslangic; i <= bitis; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+                    Console.Write(i == bitis || i == bitis - 1 ? i.ToString() : $"{i} ");
+                }
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
