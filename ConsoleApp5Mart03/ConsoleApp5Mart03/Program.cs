a1962ade203f33a9549f19fba15f18fdd3f70706
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-27 18:36:58 +0300

    for döngsü ile örüntü
    //for döngsü ile aşağıdaki gibi bir dik üçgen çizen program yazınız.
                /* 
    
                 *
                 * 
                 **
                 ***
                 ****
    
                 */

---
+++ ConsoleApp5Mart03/ConsoleApp5Mart03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleAppDers5mart03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for döngsü ile aşağıdaki gibi bir dik üçgen çizen program yazınız.
+            /* 
+
+             *
+             * 
+             **
+             ***
+             ****
+
+             */
+            Console.Write("Lütfen üçgenin yüksekliğini giriniz: ");
+            int yukseklik = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= yukseklik; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
