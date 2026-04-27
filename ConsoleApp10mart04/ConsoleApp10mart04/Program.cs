8c9991c6b5aa154146471cc5b9fc8b7787c4297c
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-27 18:45:18 +0300

    while negatif değere kadar devam etme
     // While ile kullınıcı negatif değere girene kadar kullanıcının girdiği tüm satıları toplayan program

---
+++ ConsoleApp10mart04/ConsoleApp10mart04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace ConsoleAppDers10mart04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // While ile kullınıcı negatif değere girene kadar kullanıcının girdiği tüm satıları toplayan program
+            Console.WriteLine("Negatif bir sayı girene kadar girdiğiniz tüm sayıları toplayan program");
+            int sayi = 0;
+            int toplam = 0;
+            while (sayi >= 0)
+            {
+                toplam += sayi;
+                Console.Write("Lütfen bir sayı giriniz :");
+                sayi = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            Console.WriteLine($"Toplam : {toplam}");
+            Console.ReadKey();
+        }
+    }
+}
