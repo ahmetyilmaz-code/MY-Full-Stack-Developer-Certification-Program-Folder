d4783199b6b7c8036513c5ac8848a8dbf34b34f8
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-27 18:15:33 +0300

    Operator kısaltmaları

---
+++ ConsoleApp12Subat02/ConsoleApp12Subat02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12Subat02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = 10, s2 = 20;
+            s1 = s1 + s2; //s1 = 10 + 20 = 30
+            s1 = 10; s2 = 20;
+            s1 += s2; // s1 = s1 + s2; // s1 = 10 + 20 = 30 += operatörünün kısaltılmış haliyle yazılmıştır.
+
+            s1 = 10; s2 = 20;
+            s2 = s2 - s1; // s2 = 20 - 10 = 10
+            s1 = 10; s2 = 20;
+            s2 -= s1; // s2 = s2 - s1; // s2 = 20 - 10 = 10 -= operatörünün kısaltılmış haliyle yazılmıştır.
+
+            s1 = 10; s2 = 20;
+            s1 = s1 * s2; // s1 = 10 * 20 = 200
+            s1 = 10; s2 = 20;
+            s1 *= s2; // s1 = s1 * s2; // s1 = 10 * 20 = 200 *= operatörünün kısaltılmış haliyle yazılmıştır.
+
+            s1 = 10; s2 = 20;
+            s1 = s1 / s2; // s1 = 10 / 20 = 0.5
+            s1 = 10; s2 = 20;
+            s1 /= s2; // s1 = s1 / s2; // s1 = 10 / 20 = 0.5 /= operatörünün kısaltılmış haliyle yazılmıştır.
+
+            s1 = 10; s2 = 20;
+            s1 = s1 % s2; // s1 = 10 % 20 = 10
+            s1 = 10; s2 = 20;
+            s1 %= s2; // s1 = s1 % s2; // s1 = 10 % 20 = 10 %= operatörünün kısaltılmış haliyle yazılmıştır.
+
+
+        }
+    }
+}
