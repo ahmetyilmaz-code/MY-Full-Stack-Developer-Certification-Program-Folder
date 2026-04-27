c1e7f308d5e4e47fbfb567fbda467aa77fcf6eca
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-27 18:25:55 +0300

    swicth case deneme

---
+++ ConsoleApp24Subat03/ConsoleApp24Subat03/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace subat2403
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // case alıştırma
+            Console.WriteLine("Evet mi hayır mı? (E/H)");
+            string cevap = Console.ReadLine();
+            Console.WriteLine("İf ile kontrol");
+            if (cevap == "E" || cevap == "e")
+            {
+                Console.WriteLine("Evet dediniz.");
+            }
+            else if (cevap == "H" || cevap == "h")
+            {
+                Console.WriteLine("Hayır dediniz.");
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz cevap.");
+            }
+            Console.WriteLine("--------------------");
+            Console.WriteLine("switch case ile kontrol"); // switch ife göre daha hızlı çalışır. çunku tek blokta çalışır. if ise her koşulu tek tek kontrol eder.
+            switch (cevap)
+            {
+                case "E":
+                case "e":
+                    Console.WriteLine("Evet dediniz.");
+                    break;
+                case "H":
+                case "h":
+                    Console.WriteLine("Hayır Dediniz");
+                    break;
+                default:
+                    Console.WriteLine("yanlış işlem");
+                    break;
+
+            }
+
+        }
+
+    }
