3d9ec572f86133a3e2bedc4c7095a9f9ebd3f575
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-28 16:08:03 +0300

    4 farklı metot psayı nsayı sayı metin geri dndrn
    // 4 metot yazalım
                // 1. kullanıcıdan bir metin alan ve aldığı değeri döndüren metot
                // 2. kullanıcıdan sayı alan ve değeri döndüren metot
                // 3. kullanıcıdan sadece pozitif sayı alan ve değeri döndüren metot
                // 4. kullanıcıdan sadece negatif sayı alan ve değeri döndüren metot

---
+++ ConsoleApp22nisan07/ConsoleApp22nisan07/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22Nisan07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 4 metot yazalım
+            // 1. kullanıcıdan bir metin alan ve aldığı değeri döndüren metot
+            // 2. kullanıcıdan sayı alan ve değeri döndüren metot
+            // 3. kullanıcıdan sadece pozitif sayı alan ve değeri döndüren metot
+            // 4. kullanıcıdan sadece negatif sayı alan ve değeri döndüren metot
+
+            string yazı = Metin();
+            int normalsayi = Sayi();
+            int pozitifsayi = PozitifSayi();
+            int negatifsayi = NegatifSayi();
+            Console.Clear();
+            Console.WriteLine($"1.metot metin:{yazı}\n2.metot normal sayı:{normalsayi}\n3.metot pozitif sayı:{pozitifsayi}\n4.metot negatif sayı:{negatifsayi}");
+
+
+            Console.ReadKey();
+        }
+        static string Metin()
+        {
+            Console.Write("metin giriniz :");
+            string metinn = Convert.ToString(Console.ReadLine());
+            return metinn;
+        }
+        static int Sayi()
+        {
+            Console.Write("Sayı :");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            return sayi;
+        }
+        static int PozitifSayi()
+        {
+            Console.Write("Pozitif Sayı :");
+            int Psayi = Convert.ToInt32(Console.ReadLine());
+            while (Psayi <= 0)
+            {
+                Console.Write("Pozitif sayı giriniz: ");
+                Psayi = Convert.ToInt32(Console.ReadLine());
+            }
+            return Psayi;
+        }
+        static int NegatifSayi()
+        {
+            Console.Write("negatif sayı :");
+            int Nsayi = Convert.ToInt32(Console.ReadLine());
+            while (Nsayi >= 0)
+            {
+                Console.Write("negatif sayı giriniz: ");
+                Nsayi = Convert.ToInt32(Console.ReadLine());
+            }
+            return Nsayi;
+        }
+    }
+}
