d52abc00070f4b632573633457b1303191bba386
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-28 15:18:09 +0300

    break alıştırma
     // Break alıştırma
                // kullanıcıdan en fazla 10 sayı alarak toplam yapan program.
                // kullanıcı negatif değer girdiği an program biter

---
+++ ConsoleApp17mart01/ConsoleApp17mart01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mart1701
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Break alıştırma
+            // kullanıcıdan en fazla 10 sayı alarak toplam yapan program.
+            // kullanıcı negatif değer girdiği an program biter
+            int toplam = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write($"{i}.Sayı giriniz: ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                if (sayi < 0)
+                {
+                    Console.WriteLine("Negatif değer girdiğiniz için program sonlandırıldı.");
+                    break;
+                }
+                toplam += sayi;
+            }
+            Console.WriteLine($"Toplam: {toplam}");
+        }
+    }
+}
