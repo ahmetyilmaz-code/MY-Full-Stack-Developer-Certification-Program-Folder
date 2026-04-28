1946a65ce82e58c8e032930b1c968c4ba4761bc5
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-28 16:12:14 +0300

    2 parametreli biri char diğeri int metot
    // iki parametre alan bir metot 
                // bu metot biri char biri int olmak üzere iki parametre alır
                // aldığı char değeri int değeri kadar ekrana yanyana yazan ve bir alt satıra gider.

---
+++ ConsoleApp27Nisan01/ConsoleApp27Nisan01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27Nisan01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // iki parametre alan bir metot 
+            // bu metot biri char biri int olmak üzere iki parametre alır
+            // aldığı char değeri int değeri kadar ekrana yanyana yazan ve bir alt satıra gider.
+            Console.Write("Bir işarete basınız: ");
+            char a = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            Console.Write("Bir Sayı Giriniz: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            EkranaYaz(a, b);
+
+            Console.ReadKey();
+        }
+        static void EkranaYaz(char tus, int sayı)
+        {
+            for (int i = 0; i < sayı; i++)
+            {
+                Console.Write(tus);
+            }
+            Console.WriteLine();
+        }
+
+
+
+    }
+}
