c95ebc051c06dac676159390351df7c3d919bbbe
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-28 16:06:37 +0300

    değer döndüren metot alıştırma
     // eğere bir metot değer döndüreceksee isminin hemen başına döndürülecek olan değer yazılmalı
            // örneğin kullanıcıadi isimli netot string bir değer döndüren metottur.

---
+++ ConsoleApp22nisan04/ConsoleApp22nisan04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22Nisan04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string isim = kullaniciadi();
+            string isim2 = kullaniciadi();
+            Console.WriteLine($"Merhaba {isim} ve {isim2}");
+            Console.ReadKey();
+        }
+
+        // eğere bir metot değer döndüreceksee isminin hemen başına döndürülecek olan değer yazılmalı
+        // örneğin kullanıcıadi isimli netot string bir değer döndüren metottur.
+        static string kullaniciadi()
+        {
+            Console.Write("Lütfen isminizi girin: ");
+            string isim = Console.ReadLine();
+            return isim;
+            // return ile döndürülen değer metodun türü ile aynı olmak zorundadır.
+            // Bu örnekte metodun türü string olduğu için return
+            // ile döndürülen değer de string türünde olmalıdır.
+        }
+
+
+    }
+}
