8da69189019ee7a52775f25439e157c665c3a355
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-28 15:18:56 +0300

    continue alıştırma
     // Continue alıştırma
                // kullanıcıdan en fazla 10 sayı alarak toplam yapan program.
                // kullanıcı negatif değerleri toplama dahil etmeyen program

---
+++ ConsoleApp17mart03/ConsoleApp17mart03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mart1703
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Continue alıştırma
+            // kullanıcıdan en fazla 10 sayı alarak toplam yapan program.
+            // kullanıcı negatif değerleri toplama dahil etmeyen program
+            int toplam = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write($"{i}.Sayı giriniz: ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                if (sayi < 0)
+                {
+                    Console.WriteLine("Negatif değer girdiğiniz için program bu değeri yok saydı");
+                    continue;
+                }
+                toplam += sayi;
+            }
+            Console.WriteLine($"Toplam: {toplam}");
+        }
+    }
+}
