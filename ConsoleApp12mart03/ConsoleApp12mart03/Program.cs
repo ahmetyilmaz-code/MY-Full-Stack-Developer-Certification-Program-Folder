261bba5e256bd3160ea6db8b6be9f8f1293c344a
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-28 15:12:04 +0300

    girilen sifre ve kullanıcı adı kontrol etme
     // kullanıcıdan şifre ve kullanıcı adı isteyelim ve iki değer doğru girene kadar kullanıcıdan değerleri alalım
                // Kullanıcı Adı : admin             // Şifre : 123
    
                // Kullanıcı Adı : Asa
                // Şifre : 12
                // Kullanıcı bilgileri hatalı
    
                // Kullanıcı Adı : admin
                // Şifre : 123
                // Kullanıcı bilgileri doğru Hoşgeldiniz..

---
+++ ConsoleApp12mart03/ConsoleApp12mart03/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mart1203
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // kullanıcıdan şifre ve kullanıcı adı isteyelim ve iki değer doğru girene kadar kullanıcıdan değerleri alalım
+            // Kullanıcı Adı : admin             // Şifre : 123
+
+            // Kullanıcı Adı : Asa
+            // Şifre : 12
+            // Kullanıcı bilgileri hatalı
+
+            // Kullanıcı Adı : admin
+            // Şifre : 123
+            // Kullanıcı bilgileri doğru Hoşgeldiniz..
+
+            string username = "admin", password = "123";
+            while (true)
+            {
+                string KullaniciAdi, Sifre;
+                Console.Write("Kullanıcı Adı : ");
+                KullaniciAdi = Console.ReadLine();
+                Console.Write("Şifre : ");
+                Sifre = Console.ReadLine();
+                if (KullaniciAdi == username && Sifre == password)
+                {
+                    Console.WriteLine("Kullanıcı bilgileri doğru Hoşgeldiniz..");
+                    break;
+                }
+                else if (KullaniciAdi == username && Sifre != password)
+                {
+                    Console.WriteLine("Kullanıcı şifresi hatalı");
+                }
+                else
+                {
+                    Console.WriteLine("Kullanıcı adı hatalı");
+                }
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
