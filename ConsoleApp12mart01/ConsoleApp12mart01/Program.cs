765e53272c82e57e903b4693e58b3bd5893a4b79
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-28 15:11:03 +0300

    for döngüsü ile dizi
     /*
                kullanıcıdan taban ve yükseklik alarak dötgen çizen programı yazınız 
                kullanıcı yazdırılacak simgeyide belirtecek
                Taban : 3
                Yükseklik : 4
                * * *
                * * *
                * * *
                * * *
                 */

---
+++ ConsoleApp12mart01/ConsoleApp12mart01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mart1201
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            kullanıcıdan taban ve yükseklik alarak dötgen çizen programı yazınız 
+            kullanıcı yazdırılacak simgeyide belirtecek
+            Taban : 3
+            Yükseklik : 4
+            * * *
+            * * *
+            * * *
+            * * *
+             */
+
+            Console.Write("Taban : ");
+            int taban = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Yükseklik : ");
+            int yukseklik = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Bastıralacak Simge : ");
+            char simge = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            string tabanYıldız = "";
+            for (int i = 0; i < taban; i++)
+            {
+                tabanYıldız += $"{simge} ";
+            }
+            for (int i = 0; i < yukseklik; i++)
+            {
+                Console.WriteLine(tabanYıldız);
+            }
+
+
+
+        }
+    }
+}
