abfbed35b24f2386a3b0f4fa84756ae5e9ed804f
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-28 15:24:03 +0300

    en büyük ve en küçük ortalama
    // Kullanıcıdan 10 adet sayı alarak
                // bu sayıların içerisindeki en büyük en küçük ve ortalama değeri bulan programı yazalım.

---
+++ ConsoleApp26mart02/ConsoleApp26mart02/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mart2602
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kullanıcıdan 10 adet sayı alarak
+            // bu sayıların içerisindeki en büyük en küçük ve ortalama değeri bulan programı yazalım.
+
+            int SBuyuk = 0, SKucuk = 0, Stoplam = 0, Sortalama = 0;
+            Console.WriteLine("Lütfen 10 Adet Sayı Giriniz: ");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write($"{i}.Sayıyı Giriniz: ");
+                int Sayi = Convert.ToInt32(Console.ReadLine());
+                Stoplam += Sayi;
+                Sortalama = Stoplam / i;
+                if (i == 1)
+                {
+                    SBuyuk = Sayi;
+                    SKucuk = Sayi;
+                }
+                else
+                {
+                    if (Sayi > SBuyuk)
+                    {
+                        SBuyuk = Sayi;
+                    }
+                    if (Sayi < SKucuk)
+                    {
+                        SKucuk = Sayi;
+                    }
+                }
+            }
+            Console.WriteLine($"En Büyük Sayı: {SBuyuk}");
+            Console.WriteLine($"En Küçük Sayı: {SKucuk}");
+            Console.WriteLine($"Ortalama: {Sortalama}");
+        }
+    }
+}
