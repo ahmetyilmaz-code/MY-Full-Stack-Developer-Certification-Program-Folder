7596bfb66be352daaed1b7482688094859fd55a9
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-28 15:49:48 +0300

    // Düzensiz diziler

---
+++ ConsoleApp16nisan03/ConsoleApp16nisan03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16Nisan03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Düzensiz diziler
+            int[][] duzensiz = new int[3][];
+            duzensiz[0] = new int[3];
+            duzensiz[1] = new int[2];
+            duzensiz[2] = new int[5];
+            duzensiz[0][2] = 10;
+            duzensiz[2][3] = 20;
+            Console.WriteLine(duzensiz[2][3]);
+
+
+
+        }
+    }
+}
