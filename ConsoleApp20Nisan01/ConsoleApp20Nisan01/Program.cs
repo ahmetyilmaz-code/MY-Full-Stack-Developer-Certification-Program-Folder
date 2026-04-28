0a8303717b5c5b3ec923d5959f574a3937a04e95
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-28 16:00:56 +0300

    // bir kura programı.
    // bir kura programı.
                // Kullanıcı istediği adette isimi buraya girebilir.
                // Bire bir aynı isim yazılamaz.
                // Sonra bir tuşa basılarak bir isim ratgele gelir.
                // Kullanıcı büyük harften başka tuşlayamayacığını düşünelim.
                // Bir isim bir defa gelsin kurada.

---
+++ ConsoleApp20Nisan01/ConsoleApp20Nisan01/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20Nisan01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // bir kura programı.
+            // Kullanıcı istediği adette isimi buraya girebilir.
+            // Bire bir aynı isim yazılamaz.
+            // Sonra bir tuşa basılarak bir isim ratgele gelir.
+            // Kullanıcı büyük harften başka tuşlayamayacığını düşünelim.
+            // Bir isim bir defa gelsin kurada.
+
+            Console.Write("Kaç kişik kura olacak?: ");
+            int NameAdet = Convert.ToInt32(Console.ReadLine());
+            string[] İsimler = new string[NameAdet];
+            for (int i = 0; i < İsimler.Length; i++)
+            {
+                Console.Write($"İsim {i + 1}: ");
+                string isim = Console.ReadLine();
+                Array.Sort(İsimler);
+                int index = Array.BinarySearch(İsimler, isim);
+                Array.Reverse(İsimler);
+                if (index < 0)
+                {
+                    İsimler[i] = isim;
+                }
+                else
+                {
+                    Console.WriteLine($"Bu {isim} ismi zaten var. Lütfen farklı bir isim giriniz.");
+                    i--;
+                }
+            }
+
+            Console.WriteLine("Kura için bir tuşa basınız");
+            Console.ReadKey();
+            int[] indexler = new int[İsimler.Length];
+            for (int i = 0; i < indexler.Length; i++)
+            {
+                indexler[i] = -1;
+            }
+            for (int i = 0; i < İsimler.Length; i++)
+            {
+                Console.Clear();
+                Array.Sort(indexler);
+                for (int j = 0; j < indexler.Length; j++)
+                {
+                    int indexekrankontrol = Array.BinarySearch(indexler, j);
+                    if (indexekrankontrol < -1)
+                    {
+                        Console.WriteLine(İsimler[j]);
+                    }
+                }
+                Random rnd = new Random();
+                int index = rnd.Next(0, İsimler.Length);
+                Array.Sort(indexler);
+                int indexkontrol = Array.BinarySearch(indexler, index);
+                while (indexkontrol >= -1)
+                {
+                    index = rnd.Next(0, İsimler.Length);
+                    Array.Sort(indexler);
+                    indexkontrol = Array.BinarySearch(indexler, index);
+                }
+                indexler[i] = index;
+                Console.WriteLine($"Kura sonucu: {İsimler[index]}");
+                Console.WriteLine("Yeni Kura Çekmek için bir tuşa basınız.");
+                Console.ReadKey();
+            }
+            Console.WriteLine("Tüm isimler kurada çekildi.");
+
+
+
+
+
+        }
+    }
+}
