67d7eb35dfb8367b7f2a745ff998727406dee1f6
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-28 15:19:14 +0300

    do while alıştırma

---
+++ ConsoleApp17mart04/ConsoleApp17mart04/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mart1704
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sayi = -1;
+            int toplam = 0;
+
+            while (sayi > 0) // ilk döngü negatif olduğu için hiç çalışmaz
+            {
+                Console.Write("Sayı Giriniz: ");
+                sayi = Convert.ToInt32(Console.ReadLine());
+                toplam += sayi;
+
+            }
+            Console.WriteLine($"Toplam: {toplam}");
+
+
+            do
+            {
+                Console.Write("Sayı Giriniz: ");
+                sayi = Convert.ToInt32(Console.ReadLine());
+                toplam += sayi;
+
+            } while (sayi > 0);
+            Console.WriteLine($"Toplam: {toplam}");
+
+        }
+    }
+}
