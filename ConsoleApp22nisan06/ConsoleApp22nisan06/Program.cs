a14b7c6822e8245b3592593e9796d62f5715005a
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-28 16:07:20 +0300

    2 değer alıp toplayan ve ekrana yazan metot
     // kullanıcıdan 2 sayı alarak sayıların toplamını ekrana yazan ve bulduğu
                // toplamı geri döndüren metot

---
+++ ConsoleApp22nisan06/ConsoleApp22nisan06/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22Nisan06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // kullanıcıdan 2 sayı alarak sayıların toplamını ekrana yazan ve bulduğu
+            // toplamı geri döndüren metot
+            int toplam1 = Topla();
+            int toplam2 = Topla();
+            Console.WriteLine($"total toplam = {toplam1 + toplam2}");
+            Console.ReadKey();
+        }
+        static int Topla()
+        {
+            Console.Write("Kaç adet sayı gireceksiniz: ");
+            int adet = Convert.ToInt32(Console.ReadLine());
+
+            int[] sayilar = new int[adet];
+            int toplam = 0;
+
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                Console.Write($"Lütfen {i + 1}. sayıyı giriniz: ");
+                sayilar[i] = Convert.ToInt32(Console.ReadLine());
+                toplam += sayilar[i];
+            }
+            Console.WriteLine($"Sayıların toplamı: {toplam}");
+            return toplam;
+        }
+    }
+}
