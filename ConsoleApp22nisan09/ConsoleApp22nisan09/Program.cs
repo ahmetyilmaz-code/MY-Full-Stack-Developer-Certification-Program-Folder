d1aba693c7d4bbbc4610b1065d03683627da2cd7
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-28 16:08:42 +0300

    metot parametre alıştırma
    // metotlar parametre alabilirle bunun için parantezlerin arasına istenilen parametrenin
            // türünde bir değişken tanımlanır.

---
+++ ConsoleApp22nisan09/ConsoleApp22nisan09/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22Nisan09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            EkranaYaz("deneme metot");
+            Console.ReadKey();
+        }
+        // metotlar parametre alabilirle bunun için parantezlerin arasına istenilen parametrenin
+        // türünde bir değişken tanımlanır.
+        static void EkranaYaz(string metin)
+        {
+            Console.WriteLine(metin);
+        }
+
+    }
+}
---
+++ ConsoleApp22nisan09/ConsoleApp22nisan09/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleApp22nisan09")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp22nisan09")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("73077ed0-2189-4f2b-8d40-3d5788693c20")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
