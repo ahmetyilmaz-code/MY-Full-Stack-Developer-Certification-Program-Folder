1cc7d8974b47b44bcdaea38a6d4c1519b0ebd10f
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-28 15:52:01 +0300

    Array.Clear komutu alıştırma

---
+++ ConsoleApp16nisan08/ConsoleApp16nisan08/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16Nisan08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Ahmet", "Veli", "Enes", "Fatma" };
+            foreach (var item in names)
+            {
+                Console.WriteLine($"=> {item}");
+            }
+            Console.WriteLine("Temizledikten sonra");
+            Array.Clear(names, 0, names.Length); // 0 dan başlayarak names dizisinin uzunluğu kadar elemanı temizle
+            foreach (var item in names)
+            {
+                Console.WriteLine($"=> {item}");
+            }
+        }
+    }
+}
