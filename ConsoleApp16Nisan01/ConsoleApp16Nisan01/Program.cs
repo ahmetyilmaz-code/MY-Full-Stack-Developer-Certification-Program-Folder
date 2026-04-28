2ec27bf18061a549f693daf7add8265c09617091
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-28 15:49:00 +0300

    çoklu diziler

---
+++ ConsoleApp16Nisan01/ConsoleApp16Nisan01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16Nisan01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dizi = new int[3];
+            int[] dizj = { 1, 2, 3 };
+
+            int[,] dizi2 = new int[2, 3];
+            int[,] dizj2 = { { 1, 2, 3 }, { 4, 5, 6 } };
+            foreach (int i in dizj2)
+            {
+                Console.WriteLine(i);
+            }
+
+
+        }
+    }
+}
