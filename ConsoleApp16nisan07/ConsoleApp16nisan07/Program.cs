43ee4cbc124bdd0a0b19c42095adf142fda15c56
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-28 15:51:40 +0300

    int index = Array.BinarySearch(names, "Ahmet");
     Array.BinarySearch alıştırma

---
+++ ConsoleApp16nisan07/ConsoleApp16nisan07/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16Nisan07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Ahmet", "Veli", "Enes", "Fatma" };
+            Array.Sort(names);
+            int index = Array.BinarySearch(names, "Ahmet");
+            // BinarySearch metodu sıralanmış bir dizide arama yapar
+            // ve aranan elemanın indeksini döndürür.
+            Console.WriteLine(index); //- ise aranan eleman dizide yok demektir.
+            Console.WriteLine(names[index]);
+
+        }
+    }
+}
