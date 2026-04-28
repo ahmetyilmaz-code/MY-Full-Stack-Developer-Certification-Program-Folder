c0905d94e7f3faa86fd04928a24a277c1f90273c
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-28 16:06:15 +0300

    isim alıp yazan metot
    // kullanıcıdan ismini alarak merhaba isim yazan statik ve void metot

---
+++ ConsoleApp22nisan03/ConsoleApp22nisan03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22Nisan03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // kullanıcıdan ismini alarak merhaba isim yazan statik ve void metot
+            merhaba();
+            Console.ReadKey();
+        }
+        static void merhaba()
+        {
+            Console.Write("Lütfen isminizi girin: ");
+            string isim = Console.ReadLine();
+            Console.WriteLine($"Merhaba {isim}");
+
+        }
+
+
+    }
+}
