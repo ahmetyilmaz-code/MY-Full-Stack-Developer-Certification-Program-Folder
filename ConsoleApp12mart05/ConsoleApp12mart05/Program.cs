35f15fd4618133694955d81a619464567e3c0f4c
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-28 15:13:01 +0300

    for ile dizi
    /*
                kullanıcıdan taban ve yükseklik alarak dötgen çizen programı yazınız 
                Taban : 3
                Yükseklik : 4
                * * *
                * * *
                * * *
                * * *
                 */

---
+++ ConsoleApp12mart05/ConsoleApp12mart05/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mart1205
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            kullanıcıdan taban ve yükseklik alarak dötgen çizen programı yazınız 
+            Taban : 3
+            Yükseklik : 4
+            * * *
+            * * *
+            * * *
+            * * *
+             */
+
+            Console.Write("Taban : ");
+            int taban = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Yükseklik : ");
+            int yukseklik = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < yukseklik; i++)
+            {
+                for (int j = 0; j < taban; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
