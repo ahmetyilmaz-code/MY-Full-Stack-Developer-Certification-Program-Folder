2d1e36c5e29f658488d18779f3e807f072daae97
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-28 16:09:08 +0300

    metotta çoklu parametre alıştırma

---
+++ ConsoleApp22nisan10/ConsoleApp22nisan10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22Nisan01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // metot çağrılıken parametler sırasıyla yazılarak çağrılır.
+            Metot("deneme yapıyorum", 23, true);
+            Console.ReadKey();
+        }
+        // metotlar parametre alabilirle bunun için parantezlerin arasına istenilen parametrenin
+        // türünde bir değişken tanımlanır.
+        // istenildiği sayıda parametre tanımlanabilir.
+        static void Metot(string metin, int sayı, bool doğruMu)
+        {
+            Console.WriteLine(metin);
+            if (doğruMu)
+            {
+                sayı++;
+            }
+            else
+            {
+                sayı--;
+            }
+        }
+
+    }
+}
