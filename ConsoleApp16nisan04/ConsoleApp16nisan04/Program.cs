74b9d7018aa6c29bcccd435fce7fcc791e28dfd7
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-28 15:50:13 +0300

    // Düzensiz dizi içinde dizi içinde dizi

---
+++ ConsoleApp16nisan04/ConsoleApp16nisan04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16Nisan04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Düzensiz diziler
+            int[][][] duzensiz = new int[3][][];
+            duzensiz[0] = new int[3][];
+            duzensiz[1] = new int[2][];
+            duzensiz[2] = new int[5][];
+
+            duzensiz[0][0] = new int[2];
+            duzensiz[0][1] = new int[6];
+            duzensiz[0][2] = new int[4];
+
+            duzensiz[0][0][1] = 200;
+            Console.WriteLine(duzensiz[0][0][1]);
+
+
+
+        }
+    }
+}
