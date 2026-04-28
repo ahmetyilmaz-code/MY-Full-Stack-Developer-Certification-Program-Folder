eef1821d3edb5cac4f3b05945c6633a93786ead0
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-28 16:12:38 +0300

    girilen sayının üssünü aldıran metot
     // Kuvvet ve taban değerlerini alarak üssü hepaplayan
                // ve sonucu geri döndüren metot

---
+++ ConsoleApp27nisan02/ConsoleApp27nisan02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27Nisan02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kuvvet ve taban değerlerini alarak üssü hepaplayan
+            // ve sonucu geri döndüren metot
+            Console.Write("Taban değer: ");
+            int Taban = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Kuvvet değeri: ");
+            int Kuvvet = Convert.ToInt32(Console.ReadLine());
+            int sonuc = Power(Taban, Kuvvet);
+            Console.WriteLine($"Sonuc :{sonuc}");
+            Console.ReadKey();
+        }
+        static int Power(int taban, int kuvvet)
+        {
+            int sonuc = 1;
+            for (int i = 0; i < kuvvet; i++)
+            {
+                sonuc *= taban;
+            }
+            return sonuc;
+        }
+
+
+    }
+}
---
+++ ConsoleApp27nisan02/ConsoleApp27nisan02/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleApp27nisan02")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp27nisan02")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8f8e01c2-1277-434f-9e72-56613b95edf0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
