7303ad6b1ea779a4d587713c052a48002629312d
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-04-28 15:49:31 +0300

    öürüntü diziler ile ve foreach ile
    //***
                //***
                //*** foreach ile
                //string[,] satirlar = new string[3, 4]

---
+++ ConsoleApp16nisan02/ConsoleApp16nisan02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16Nisan02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //***
+            //***
+            //*** foreach ile
+            //string[,] satirlar = new string[3, 4]            
+            string[,] satirlar =
+            {
+                { "*", "*", "*", "\n" },
+                { "*", "*", "*", "\n" },
+                { "*", "*", "*", "\n" }
+            };
+
+            foreach (var i in satirlar)
+            {
+                Console.Write(i);
+            }
+        }
+    }
+}
