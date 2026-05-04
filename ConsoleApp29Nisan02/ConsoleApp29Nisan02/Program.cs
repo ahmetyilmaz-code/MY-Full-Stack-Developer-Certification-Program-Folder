a9e17eafdc49c92b9ea298ff0bea73a3b406ad8e
ahmetyilmaz-code <lancerahmet@gmail.com>
2026-05-04 17:06:50 +0300

    metotlarda ref out
    //SayıAl("1. Sayı: ",int) bu metot iki parametr alır birisi string birisi ref veya out
                // int string olanı etiket  olarak ekrana basar
                // int olan ise kullanıcıdan alınan değer olur.

---
+++ ConsoleApp29Nisan02/ConsoleApp29Nisan02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29nisan02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //SayıAl("1. Sayı: ",int) bu metot iki parametr alır birisi string birisi ref veya out
+            // int string olanı etiket  olarak ekrana basar
+            // int olan ise kullanıcıdan alınan değer olur.
+            int sayi;
+            string metin = "1.Sayı: ";
+            SayıAl(metin, out sayi);
+            Console.Write($"{metin}{sayi}");
+            //Console.WriteLine($"{SayıAl(metin)}");
+        }
+
+        static void SayıAl(string metin, out int sayi)
+        {
+            Console.Write(metin);
+            sayi = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+    }
+}
---
+++ ConsoleApp29Nisan02/ConsoleApp29Nisan02/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("ConsoleApp29Nisan02")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp29Nisan02")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("77316eb6-f9f3-4572-8090-4427964e3b20")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
