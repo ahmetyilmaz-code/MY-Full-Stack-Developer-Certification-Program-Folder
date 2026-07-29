fbfb51540f760fdaa75e87a57c8cb00a9a35c484
ahmet yılmaz <lancerahmet@gmail.com>
2026-07-29 15:17:16 +0300

    Generic(ŞablonTip) örnek

---
+++ ConsoleApp8temmuz02/ConsoleApp8temmuz02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8temmuz02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dnm<int> dnm = new dnm<int>();
+            dnm.dnmOzllik = 120;
+            int sayi = dnm.DnmMetot();
+
+            dnm<string> dnm1 = new dnm<string>();
+            dnm1.dnmOzllik = "Yazi";
+            string yazi = dnm1.DnmMetot();
+
+            Console.ReadLine();
+        }
+
+        class dnm<T>
+        {
+            public T dnmOzllik { get; set; }
+            public T DnmMetot()
+            {
+                return dnmOzllik;
+            }
+        }
+
+    }
+
+
+
+
+
+}
+
