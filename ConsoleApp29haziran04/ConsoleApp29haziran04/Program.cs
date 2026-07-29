4ea7681b6a346142786448679c841f066aae0046
ahmet yılmaz <lancerahmet@gmail.com>
2026-07-29 15:00:41 +0300

    sınıflarda kalıtım

---
+++ ConsoleApp29haziran04/ConsoleApp29haziran04/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29haziran04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogrenci = new Ogrenci();
+            ogrenci.Ad = "asdasd";
+            ogrenci.Soyad = "wad";
+            ogrenci.Sinifi = "asda";
+
+            Ogretmen ogretmen = new Ogretmen();
+            ogretmen.Ad = "asd";
+            ogretmen.Brans = "asd";
+
+        }
+
+        // Kalıtım 
+        class Ogrenci : Kullanici
+        {
+
+            public string Sinifi { get; set; }
+        }
+        class Ogretmen : Kullanici
+        {
+
+            public string Brans { get; set; }
+        }
+        class Kullanici
+        {
+            public string KullaniciAdi { get; set; }
+            public string Sifre { get; set; }
+            public string Ad { get; set; }
+            public string Soyad { get; set; }
+        }
+    }
+}
