9214d913d7f940ba1a938c3ba177ee5449d36829
ahmet yılmaz <lancerahmet@gmail.com>
2026-07-29 15:18:28 +0300

    şablon tip generik kısıtlama kalıtım ve interface

---
+++ ConsoleApp8temmuz07/ConsoleApp8temmuz07/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8temmuz07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            B<A> b = new B<A>();
+            b.dnmOzllik1 = new A();
+
+            Console.ReadLine();
+        }
+        interface Iinterface
+        {
+
+        }
+        class A : Iinterface
+        {
+
+        }
+        class B<U> where U : Iinterface
+        {
+            public U dnmOzllik1 { get; set; }
+        }
+    }
+}
+
