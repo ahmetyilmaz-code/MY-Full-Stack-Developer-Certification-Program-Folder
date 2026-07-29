08384fb3149cfbf9beb30ccef402ceabf51a8441
ahmet yılmaz <lancerahmet@gmail.com>
2026-07-29 14:56:14 +0300

    string fitlreler örnek 2 nextmatch ile

---
+++ ConsoleApp24haziran05/ConsoleApp24haziran05/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Permissions;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24haziran05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sayilar = "12,23,4,5,,,34,6,61,,37";
+            string filtre = @"\d+";
+            Regex regex = new Regex(filtre);
+            Match match = regex.Match(sayilar);
+            int toplam = 0;
+            while (match.Success)
+            {
+                toplam += Convert.ToInt32(match.Value);
+                match = match.NextMatch();
+            }
+            Console.WriteLine(toplam);
+
+            Console.ReadKey();
+        }
+
+    }
+}
