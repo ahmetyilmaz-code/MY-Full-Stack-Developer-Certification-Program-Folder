dfbd9e5093d9a79ba0fbe55c0a2967f3606ccbe4
ahmet yılmaz <lancerahmet@gmail.com>
2026-07-29 14:56:31 +0300

    regex replace

---
+++ ConsoleApp24haziran06/ConsoleApp24haziran06/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Permissions;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24haziran06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string metin = "Ahmet Iban: TR 1234 1234 1234 23";
+            string filtre = @"TR \d{4} \d{4} \d{4} \d{2}";
+            Regex regex = new Regex(filtre);
+            string sansurlu = regex.Replace(metin, "********");
+            Console.WriteLine(sansurlu);
+
+            Console.ReadKey();
+        }
+    }
+}
