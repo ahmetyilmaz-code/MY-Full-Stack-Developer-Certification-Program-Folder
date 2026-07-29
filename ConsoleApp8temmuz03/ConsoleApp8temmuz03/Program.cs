1670adc9854faadd30ae78504158da00494a802e
ahmet yılmaz <lancerahmet@gmail.com>
2026-07-29 15:17:32 +0300

    sablon tip ornek 2

---
+++ ConsoleApp8temmuz03/ConsoleApp8temmuz03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8temmuz03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dnm<int, char> dnm = new dnm<int, char>();
+            dnm.dnmOzllik1 = 12;
+            dnm.dnmOzllik2 = 'u';
+
+            Console.ReadLine();
+        }
+
+        class dnm<T, U>
+        {
+            public T dnmOzllik1 { get; set; }
+            public U dnmOzllik2 { get; set; }
+
+        }
+
+    }
+}
+
