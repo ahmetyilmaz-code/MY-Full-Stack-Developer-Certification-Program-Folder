8a18722f8ded54339c7eb006ace5e66eba27216d
ahmet yılmaz <lancerahmet@gmail.com>
2026-07-29 15:18:09 +0300

    şablon tip Generic kısıtlama (Generic Constraint)

---
+++ ConsoleApp8temmuz06/ConsoleApp8temmuz06/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8temmuz06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            B<int> b = new B<int>();
+            b.dnmOzllik1 = 1;
+
+            Console.ReadLine();
+        }
+
+
+        class B<U> where U : struct
+        {
+            public U dnmOzllik1 { get; set; }
+        }
+    }
+}
+
