d0ece64418e0b9a95a04c1643972c4c8245ee077
ahmet yılmaz <lancerahmet@gmail.com>
2026-07-29 15:17:53 +0300

    şablon tip interface ile

---
+++ ConsoleApp8temmuz05/ConsoleApp8temmuz05/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8temmuz05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            B<string> b = new B<string>();
+            b.dnmOzllik1 = "1";
+
+            Console.ReadLine();
+        }
+
+        interface IA<T>
+        {
+            T dnmOzllik1 { get; set; }
+        }
+        class B<U> : IA<U>
+        {
+            public U dnmOzllik1 { get; set; }
+        }
+    }
+}
+
