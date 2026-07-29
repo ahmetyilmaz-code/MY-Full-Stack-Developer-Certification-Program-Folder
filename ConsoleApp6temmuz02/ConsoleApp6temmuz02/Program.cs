ced535682bc1340914c3166fe08bca4fb2d1ae09
ahmet yılmaz <lancerahmet@gmail.com>
2026-07-29 15:06:00 +0300

    try catch

---
+++ ConsoleApp6temmuz02/ConsoleApp6temmuz02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6temmuz02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sonuc = 0;
+            int sayi1 = Sayial();
+            int sayi2 = Sayial();
+
+
+        }
+        public static int Sayial()
+        {
+            int sayi = 0;
+            try
+            {
+                Console.WriteLine("Sayı :");
+                sayi = Convert.ToInt32(Console.ReadLine());
+
+            }
+            catch
+            {
+
+            }
+
+
+            return sayi;
+        }
+
+
+    }
+}
+
