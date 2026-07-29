0cf05fcb2afe27292fe08fc21dc6a0ea9aa6e692
ahmet yılmaz <lancerahmet@gmail.com>
2026-07-29 14:48:29 +0300

    string komutlar örnek
                // Kullanici adında bir sınıf olacak.
                // Bu sınıfta KullanıcıAdı, Şifre ve KullanıcıNumarası olacak şekilde 3 özellik olacak.
                // Kullanici kullanici = " BehlulMese ,1234, 9019   ";
                // kullanıcı.KullanıcıAdı = "BehlulMese"
                // kullanıcı.Sifre = "1234"
                // string kullanıcıBilgileri = (string)kullanici;
                // "Kullanıcı Adı: BehlulMese\nŞifre: 1234\nKullanıcı Numarası: 9019"

---
+++ ConsoleApp22haziran02/ConsoleApp22haziran02/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22haizran02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Ödev
+            // Kullanici adında bir sınıf olacak.
+            // Bu sınıfta KullanıcıAdı, Şifre ve KullanıcıNumarası olacak şekilde 3 özellik olacak.
+            // Kullanici kullanici = " BehlulMese ,1234, 9019   ";
+            // kullanıcı.KullanıcıAdı = "BehlulMese"
+            // kullanıcı.Sifre = "1234"
+            // string kullanıcıBilgileri = (string)kullanici;
+            // "Kullanıcı Adı: BehlulMese\nŞifre: 1234\nKullanıcı Numarası: 9019"
+
+            Kullanici kullanici = " BehlulMese ,1234, 9019   ";
+            Console.WriteLine(kullanici.KullaniciAdi);
+            Console.WriteLine(kullanici.Sifre);
+            Console.WriteLine(kullanici.KullaniciNumarasi);
+
+
+            string kullaniciBilgileri = (string)kullanici;
+            Console.WriteLine();
+            Console.WriteLine(kullaniciBilgileri);
+
+
+
+        }
+
+        class Kullanici
+        {
+            public string KullaniciAdi { get; set; }
+            public string Sifre { get; set; }
+            public string KullaniciNumarasi { get; set; }
+
+            // string -> Kullanici
+            public static implicit operator Kullanici(string deger) // implicit - bilinçli 
+            {
+                string[] bilgiler = deger.Split(',');
+
+                return new Kullanici
+                {
+                    KullaniciAdi = bilgiler[0].Trim(),
+                    Sifre = bilgiler[1].Trim(),
+                    KullaniciNumarasi = bilgiler[2].Trim()
+                };
+            }
+
+
+            // Kullanici -> string
+            public static explicit operator string(Kullanici kullanici) // explicit - bilinçsiz
+            {
+                return $"Kullanıcı Adı: {kullanici.KullaniciAdi}\nŞifre: {kullanici.Sifre}\nKullanıcı Numarası: {kullanici.KullaniciNumarasi}";
+            }
+
+        }
+
+
+
+    }
+}
---
+++ ConsoleApp22haziran02/ConsoleApp22haziran02/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleApp22haziran02")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp22haziran02")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("896a8df7-7f53-448c-9775-7b769ae091f8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
